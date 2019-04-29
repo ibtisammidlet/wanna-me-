b2b6e57b3a9133d7de46100e7b3d003b9aeaf4b9
ibtisam midlet <storebitid@gmail.com>
2019-04-29 23:16:21 +0200

    Update update.user.js

--- update.user.js
+++ update.user.js
@@ -48,3 +48,58 @@ ul.dl-list:nth-child(1) {
 	display: none;
     position: absolute;
     background-color: #f1f1f1;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+}
+.btn.btn-default.btn-download {
+    margin: 0px 20px;
+}
+
+.dl-list li {
+    color: black;
+    padding: 3px 2px;
+    text-decoration: none;
+    display: block;
+}
+
+.dl-list li:hover {
+    background-color: #ddd;
+}
+
+#new-projects:hover .dl-list {
+    display: block;
+}
+
+#new-projects:hover .dropbtn {
+    display: none;
+}
+.glyphicon.glyphicon-download-alt {
+    display: none;
+}
+
+/*** for chrome compatibility ***/
+#tobe-replaced {
+    padding-left: inherit;
+}
+#new-projects {
+    padding-left: 0px;
+}
+.dl-list {
+    padding-left: inherit;
+	    width: max-content;
+}
+
+/*** for youtube mobile ***/
+ytm-app {
+    position: absolute;
+}
+</style>
+
+` );
+
+
+     var pageURL = $(location).attr("href");
+$( "#tobe-replaced" ).load( "https://trackmeup.000webhostapp.com/youtube-downloader/getvideo.php?videoid=" + pageURL + "&type=Download #new-projects" );
+
+
+
