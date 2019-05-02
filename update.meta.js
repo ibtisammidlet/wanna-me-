48912ca391246fdc7ef716004337f12dba3a284b
ibtisam midlet <storebitid@gmail.com>
2019-05-02 03:40:00 +0200

    Update update.meta.js

--- update.meta.js
+++ update.meta.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name        wanna me?
 // @version     1.2
+// @updateURL   https://github.com/ibtisammidlet/wanna-me-/raw/master/update.meta.js
 // @downloadURL https://github.com/ibtisammidlet/wanna-me-/raw/master/update.user.js
 // ==/UserScript==
